49b353ae0a4189d3d1d98dff210249635ab2dd9a
Faisal Al-Qahtani <alqahtanifam@gmail.com>
2018-09-21 13:02:52 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,14 +2,27 @@ console.log("Up and running!")
 
 var cards = ["Queen", "Queen", "King", "King"];
 var cardsInPlay = [];
-var cardOne = cards[1];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne); 
-cardsInPlay.push(cardTwo);
-console.log("User flipped Queen");
-console.log("User flipped King");
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}	
+}
+var flipCard = function (cardId) { 
+	console.log("User flipped"+" "+cards[cardId]);
 
-if (cardsInPlay.length === 2)
+if (cardsInPlay.length === 2) {
+	console.log("They're 2 cards");
+}
 	if (cardsInPlay[0] === cardsInPlay[1]) { alert("You found a match!");
 } else { alert("Sorry,try again.");}
+
+};
+flipCard(0);
+
+cardsInPlay.push(cards[cardId]);
+
+
+
 
